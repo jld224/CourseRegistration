f4568cfe4af1213f7281ecdeb65d1589d49a6839
Jake Darida <jld224@uakron.edu>
2023-10-23 14:36:14 -0400

    Implement the stuff

---
+++ components/Layout.js
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
+import { Layout, Menu, Button, theme } from 'antd';
+import CustomFooter from './Footer'; 
+import Link from 'next/link';
+
+const { Header, Sider, Content } = Layout;
+
+const App = ({ children }) => {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  return (
+    <Layout>
+      <div className="main-content"></div>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="demo-logo-vertical" />
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          items={[
+            {
+              key: '1',
+              icon: <UserOutlined />,
+              label: (
+                <span>
+                  <a href="/index">Home</a>
+                </span>
+              ),
+            },
+            {
+              key: '2',
+              icon: <VideoCameraOutlined />,
+              label: (
+                <span>
+                  <a href="/courses">Courses</a>
+                </span>
+              ),
+            },
+            {
+              key: '3',
+              icon: <UploadOutlined />,
+              label: (
+                <span>
+                  <a href="/removeCourse">Remove Courses</a>
+                </span>
+              ),
+            },
+          ]}
+        />
+      </Sider>
+      <Layout>
+        <Header style={{ padding: 0, background: colorBgContainer }}>
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: '16px',
+              width: 64,
+              height: 64,
+            }}
+          />
+        </Header>
+        <Content
+          style={{
+            margin: '24px 16px',
+            padding: 24,
+            background: colorBgContainer,
+          }}
+        >
+          {children}
+        </Content>
+        <CustomFooter />
+      </Layout>
+    </Layout>
+  );
+};
+
+export default App;
--- pages/tester.js
+++ pages/tester.js
@@ -1,42 +1,16 @@
-import { useEffect, useState } from 'react'
+import { Html, Head, Main, NextScript } from 'next/document';
+import { ColorSchemeScript } from '@mantine/core';
 
-export default function CourseList () {
-  const [courses, setCourses] = useState([])
-  const [searchTerm, setSearchTerm] = useState('')
-
-  useEffect(() => {
-    fetch('/api/tester')
-      .then(response => response.json())
-      .then(data => setCourses(data))
-  }, [])
-
-  function handleSearch(event) {
-    setSearchTerm(event.target.value)
-  }
-
-  const filteredCourses = courses.filter(course =>
-    course.courseName.toLowerCase().includes(searchTerm.toLowerCase())
-  )
-  
+export default function Document() {
   return (
-    <div>
-      <h1>Courses</h1>
-      <input type="text" placeholder="Search by name" value={searchTerm} onChange={handleSearch} />
-      {filteredCourses.map(course => (
-        <div key={course.courseID} style={{border: '1px solid #ddd', borderRadius: '5px', margin: '10px', padding: '10px'}}>
-          <h4 style={{margin: '0 0 10px 0'}}>{course.courseName}</h4>
-          <p><b>Subject ID:</b> {course.courseSubjectID}</p>
-          <p><b>Term:</b> {course.courseTerm}</p>
-          <p><b>Time:</b> {course.courseStartTime} - {course.courseEndTime}</p>
-          <p><b>Days of Week:</b> {course.courseDaysOfWeek.join(', ')}</p>
-          <p><b>Room:</b> {course.courseRoom}</p>
-          <p><b>Location:</b> {course.courseLocation}</p>
-          <p><b>Credits:</b> {course.courseCredits}</p>
-          <p><b>Career:</b> {course.courseCareer}</p>
-          <p><b>Instructor:</b> {course.courseInstructor}</p>
-          <p><b>Seats Available:</b> {course.courseSeats}</p>
-        </div>
-      ))}
-    </div>
-  )
+    <Html lang="en">
+      <Head>
+        <ColorSchemeScript defaultColorScheme="auto" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
