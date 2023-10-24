c5d1f40a7eb76713965a2c5a40e2c55b89787600
Jake Darida <jld224@uakron.edu>
2023-10-24 19:05:39 -0400

    add the register and login and stuff

--- pages/login.js
+++ pages/login.js
@@ -1,48 +1,63 @@
 import { useState } from 'react';
-import { Form, Input, Button, message } from 'antd';
+import { Form, Input, Button, message, Spin } from 'antd';
+import { useRouter } from 'next/router';
 
 export default function LoginPage() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const router = useRouter();
 
-    const handleLogin = async () => {
-        if (!email.trim() || !password.trim()) {
-            message.error("Email and password must be filled out.");
-            return;
-        }
-        const response = await fetch('/api/login', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ email, password })
-        });
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
-        if (!response.ok) {
-            console.error('API request failed', response.status, response.statusText);
-            return;
-        }
+  const handleLogin = async () => {
+    setIsLoading(true);
+    if (!email.trim() || !password.trim()) {
+      message.error("Email and password must be filled out.");
+      setIsLoading(false);
+      return;
+    }
 
-        const result = await response.json();
-        if (result.success) {
-            message.success('Logged in successfully!');
-        } else {
-            message.error(result.message);
-        }
-    };
+    const response = await fetch('/api/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email, password }),
+    });
 
-    return (
-        <div>
-            <h1>Login</h1>
-            <Form layout="vertical" onFinish={handleLogin}>
-                <Form.Item label="Email" name="email" rules={[{ required: true, message: 'Email is required' }]}>
-                    <Input value={email} onChange={(e) => setEmail(e.target.value)} />
-                </Form.Item>
-                <Form.Item label="Password" name="password" rules={[{ required: true, message: 'Password is required' }]}>
-                    <Input.Password value={password} onChange={(e) => setPassword(e.target.value)} />
-                </Form.Item>
-                <Form.Item>
-                    <Button type="primary" htmlType="submit">Login</Button>
-                </Form.Item>
-            </Form>
-        </div>
-    );
+    setIsLoading(false);
+
+    if (!response.ok) {
+      console.error('API request failed', response.status, response.statusText);
+      return;
+    }
+
+    const result = await response.json();
+
+    if (result.success) {
+      message.success('Logged in successfully!');
+      router.push('/');
+    } else {
+      message.error(result.message);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <Form layout="vertical" onFinish={handleLogin}>
+        <Form.Item label="Email" name="email" rules={[{ required: true, message: 'Email is required' }]}>
+          <Input value={email} onChange={(e) => setEmail(e.target.value)} />
+        </Form.Item>
+        <Form.Item label="Password" name="password" rules={[{ required: true, message: 'Password is required' }]}>
+          <Input.Password value={password} onChange={(e) => setPassword(e.target.value)} />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit" loading={isLoading}>
+             Login
+          </Button>
+        </Form.Item>
+      </Form>
+
+      {isLoading && <Spin />}
+    </div>
+  );
 }
